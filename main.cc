7d78561b8f60e755e9cb100169915448c7ea86fc
victor <baessa60@gmail.com>
2021-09-09 16:39:42 -0300

    Addition of StaDevices

--- main.cc
+++ main.cc
@@ -19,6 +19,23 @@
 #include "ns3/mobility-module.h"
 #include "ns3/yans-wifi-helper.h"
 #include "ns3/ssid.h"
+#include "ns3/athstats-helper.h"
+
+
+#include "ns3/command-line.h"
+#include "ns3/config.h"
+#include "ns3/boolean.h"
+#include "ns3/string.h"
+#include "ns3/yans-wifi-helper.h"
+#include "ns3/ssid.h"
+#include "ns3/mobility-helper.h"
+#include "ns3/on-off-helper.h"
+#include "ns3/yans-wifi-channel.h"
+#include "ns3/mobility-model.h"
+#include "ns3/packet-socket-helper.h"
+#include "ns3/packet-socket-address.h"
+#include "ns3/athstats-helper.h"
+
 
 using namespace ns3;
 
@@ -60,29 +77,39 @@ main (int argc, char *argv[])
   Packet::EnablePrinting ();
   
   uint32_t nAp = 4;
+  uint32_t nStas = 3;
   
   NodeContainer ap; 
   ap.Create (nAp);
 
+  NodeContainer stas; 
+  stas.Create (nStas);
+
   PacketSocketHelper packetSocket;
   packetSocket.Install (ap);
+  packetSocket.Install (stas);
   
   MobilityHelper mobility;
   mobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
   mobility.Install (ap);
+  mobility.Install (stas);
 
   SetPosition(ap.Get(0), Vector(1.0, 1.0, 1.0));
-  SetPosition(ap.Get(1), Vector(11.0, 1.0, 1.0));
-  SetPosition(ap.Get(2), Vector(11.0, 11.0, 1.0));
-  SetPosition(ap.Get(3), Vector(1.0, 11.0, 1.0));
+  SetPosition(ap.Get(1), Vector(1.0, 5.0, 1.0));
+  SetPosition(ap.Get(2), Vector(5.0, 5.0, 1.0));
+  SetPosition(ap.Get(3), Vector(5.0, 1.0, 1.0));
 
+  SetPosition(stas.Get(0), Vector(2.0, 1.0, 1.0));
+  SetPosition(stas.Get(1), Vector(2.0, 5.0, 1.0));
+  SetPosition(stas.Get(2), Vector(4.0, 4.0, 1.0));
+  
   //Building creation
   double x_min = 0.0;
-  double x_max = 20.0;
+  double x_max = 6.0;
   double y_min = 0.0;
-  double y_max = 20.0;
+  double y_max = 6.0;
   double z_min = 0.0;
-  double z_max = 10.0;
+  double z_max = 3.0;
   
   Ptr<Building> b = CreateObject <Building> ();
 
@@ -93,12 +120,10 @@ main (int argc, char *argv[])
   b->SetNRoomsX (2);
   b->SetNRoomsY (2);
 
-
-  BuildingsHelper::Install (ap);
     
   Config::SetDefault ("ns3::WifiRemoteStationManager::RtsCtsThreshold", StringValue ("2200"));
   Config::SetDefault ("ns3::WifiRemoteStationManager::FragmentationThreshold", StringValue ("2200"));
-  Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode", StringValue ("OfdmRate6MbpsBW10MHz"));
+  //Config::SetDefault ("ns3::WifiRemoteStationManager::NonUnicastMode", StringValue ("OfdmRate6MbpsBW10MHz"));
 
   YansWifiChannelHelper wifiChannel= YansWifiChannelHelper::Default ();
   wifiChannel.SetPropagationDelay ("ns3::ConstantSpeedPropagationDelayModel");
@@ -106,31 +131,55 @@ main (int argc, char *argv[])
   Ptr<HybridBuildingsPropagationLossModel> propagationLossModel = CreateObject<HybridBuildingsPropagationLossModel> ();
   wifiChannel.AddPropagationLoss("ns3::HybridBuildingsPropagationLossModel",
                                  "CitySize", StringValue("Small"),
-                                 "ShadowSigmaOutdoor", DoubleValue (10.0),
-                                 "ShadowSigmaExtWalls", DoubleValue (10.0),
-                                 "InternalWallLoss", DoubleValue (10.0),
+                                 "ShadowSigmaOutdoor", DoubleValue (7.0),
+                                 "ShadowSigmaIndoor", DoubleValue (5.0),
+                                 "ShadowSigmaExtWalls", DoubleValue (5.0),
+                                 "InternalWallLoss", DoubleValue (5.0),
                                  "Environment", StringValue("Urban"));
 
   YansWifiPhyHelper wifiPhy;
   wifiPhy.SetChannel (wifiChannel.Create ());
 
+  WifiHelper wifi;
   WifiMacHelper wifiMac;
   Ssid ssid = Ssid ("wifi-default");
-  wifiMac.SetType ("ns3::ApWifiMac",
-      "Ssid", SsidValue (ssid),
-      "BeaconInterval", TimeValue (MicroSeconds (2560000.0)));
-    
-  WifiHelper wifi;
-  
   wifi.SetRemoteStationManager ("ns3::ArfWifiManager");
+
+  //setup stas
+  wifiMac.SetType ( "ns3::StaWifiMac",
+                    "ActiveProbing", BooleanValue(true),
+                    "Ssid", SsidValue(ssid));
+  
+  NetDeviceContainer staDevs = wifi.Install(wifiPhy, wifiMac, stas);
+
+  //setup ap
+  wifiMac.SetType ( "ns3::ApWifiMac",
+                    "Ssid", SsidValue (ssid),
+                    "BeaconInterval", TimeValue (MicroSeconds (2560000.0)));
 
   wifi.Install (wifiPhy, wifiMac, ap);
   
 
-  Simulator::Stop (Seconds (25.0));
-  Config::Connect ("/NodeList/*/DeviceList/*/Phy/State/RxOk", MakeCallback (&PhyRxOkTrace));
-  Config::Connect ("/NodeList/*/DeviceList/*/Phy/State/Tx", MakeCallback (&PhyTxTrace));
+  PacketSocketAddress socket;
+  socket.SetSingleDevice (staDevs.Get(0)->GetIfIndex());
+  socket.SetPhysicalAddress (staDevs.Get(1)->GetAddress());
+  socket.SetProtocol(1);
 
+  OnOffHelper onoff("ns3::PacketSocketFactory", Address(socket));
+  onoff.SetConstantRate(DataRate("500kb/s"));
+
+  ApplicationContainer apps = onoff.Install(stas.Get(0));
+  apps.Start(Seconds(2));
+  apps.Stop(Seconds(24.0));
+
+  BuildingsHelper::Install (ap);
+  BuildingsHelper::Install (stas);
+
+  Simulator::Stop (Seconds (25.0));
+
+  Config::Connect ("/NodeList/3/DeviceList/*/Phy/State/RxOk", MakeCallback (&PhyRxOkTrace));
+  //Config::Connect ("/NodeList/*/DeviceList/*/Phy/State/Tx", MakeCallback (&PhyTxTrace));
+  
   Simulator::Run ();
 
   Simulator::Destroy ();
